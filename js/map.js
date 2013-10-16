316a4fb65a845136d38b44e423ab6636d2b434de
joshuasouders <rem87062@yahoo.com>
2013-10-16 18:13:28 -0400

    gui design

---
+++ js/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([42.2, -75.2319], 8);
+
+L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
