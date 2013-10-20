c3ce640acf764c50079f712aa96018ad7aa88d2a
joshuasouders <rem87062@yahoo.com>
2013-10-20 18:53:27 -0400

    adding panel components

--- js/map.js
+++ js/map.js
@@ -2,7 +2,11 @@ var map = L.map('map', {
 	measureControl: true
 }).setView([42.2, -75.2319], 8);
 
+L.control.scale({
+	maxWidth: 300
+}).addTo(map);
+
 L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18
 }).addTo(map);
--- js/panel.js
+++ js/panel.js
@@ -1,1 +1,37 @@
 $('.dropdown-toggle').dropdown();
+$('#slider').slider()
+  .on('slide', function(ev){
+
+  });
+
+
+$('#dropdown-options-data-classification li').on('click', function() {
+    $('#dropdown-title-data-classification').html($(this).find('a').html());
+});
+
+$('#dropdown-options-state li').on('click', function() {
+    $('#dropdown-title-state').html($(this).find('a').html());
+});
+
+$('#dropdown-options-city li').on('click', function() {
+    $('#dropdown-title-city').html($(this).find('a').html());
+});
+
+
+var piechart = new GeoDash.PieChart('.piechart', {
+  label: 'source',
+  value: 'percent',
+  colors: ["#0B6909", "#d80000"],
+  innerRadius: 10,
+  hover:true
+});
+
+var data = [{
+    "source":"Met",
+    "percent":68
+  },{
+    "source":"Unmet",
+    "percent":32
+  }];
+
+piechart.update(data);
